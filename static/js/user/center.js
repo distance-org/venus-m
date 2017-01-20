be80674412ab40a64a3e4541e9c615db44b9922c
guoyang <guoyang@doumi.com>
2017-01-20 11:18:57 +0800

    accumulate

---
+++ static/js/user/center.js
@@ -0,0 +1,11 @@
+import StatusBarB from '/common/ui_widget/status_bar_b/index.js'
+new Vue({
+    el : '#container',
+    components : {
+        StatusBarB,
+    },
+    data : {
+    },
+    methods : {
+    }
+});
